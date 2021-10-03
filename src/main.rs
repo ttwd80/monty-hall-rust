5bc6f4d0882a04c01ed47f643525f89555dfa8c4
Titi Wangsa bin Damhore <ttwd80@gmail.com>
2021-10-04 02:59:46 +1100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,62 @@
 use rand::Rng;
 
 const COUNT_PER_SET: i32 = 1000;
+const TOTAL_SET: i32 = 10;
 
 fn main() {
-    let mut count = 0;
-    for _i in 0..COUNT_PER_SET {
-        count += simulate(true);
+    for _n in 0..TOTAL_SET {
+        process_set(rand::random());
+        println!("***");
     }
-    println!("If you stay, chances of winning are {:.3}%", (count as f64 / COUNT_PER_SET as f64 * 100.00));
+}
+fn process_change() {
+    let mut change_count = 0;
+    for _i in 0..COUNT_PER_SET {
+        change_count += simulate(false);
+    }
+    println!(
+        "If you change, chances of winning are {:.3}%",
+        (change_count as f64 / COUNT_PER_SET as f64 * 100.00)
+    );
+}
+fn process_stay() {
+    let mut stay_count = 0;
+    for _i in 0..COUNT_PER_SET {
+        stay_count += simulate(true);
+    }
+    println!(
+        "If you stay, chances of winning are {:.3}%",
+        (stay_count as f64 / COUNT_PER_SET as f64 * 100.00)
+    );
+}
+
+fn process_set(stay_first: bool) {
+    if stay_first {
+        process_stay();
+        process_change();
+    } else {
+        process_change();
+        process_stay();
+    }
 }
 
 fn simulate(stay: bool) -> i32 {
     let mut rng = rand::thread_rng();
 
     let car_location = rng.gen_range(0..3);
-    println!("We have Door#1, Door#2, Door#3");
-    println!("The car is behind Door#{}", car_location + 1);
+    //println!("We have Door#1, Door#2, Door#3");
+    //println!("The car is behind Door#{}", car_location + 1);
 
     let picked_location = rng.gen_range(0..3);
-    println!("We have chosen Door#{}", picked_location + 1);
+    //println!("We have chosen Door#{}", picked_location + 1);
 
     let pick: char;
 
     if car_location == picked_location {
-        println!("(Good pick!)");
+        //println!("(Good pick!)");
         pick = 'G';
     } else {
-        println!("(Bad pick!)");
+        //println!("(Bad pick!)");
         pick = 'B';
     }
 
@@ -41,7 +71,7 @@ fn simulate(stay: bool) -> i32 {
         reveal = bad_doors[(reveal_index + 1) % 2]
     }
 
-    println!("Reveal: Door#{} is bad!", reveal + 1);
+    //println!("Reveal: Door#{} is bad!", reveal + 1);
 
     if stay && pick == 'G' {
         return 1;
